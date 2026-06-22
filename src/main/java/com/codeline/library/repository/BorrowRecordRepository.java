d9797731f1c1be1443c67e200dc149f4dc825149
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 13:49:51 +0400

    Created BorrowRecordRepository

---
+++ src/main/java/com/codeline/library/repository/BorrowRecordRepository.java
@@ -0,0 +1,7 @@
+package com.codeline.library.repository;
+
+import com.codeline.library.entity.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+}
