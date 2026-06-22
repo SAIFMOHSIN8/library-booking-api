2d2cce0e88777f118682c8ab14dd106e4c97b770
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 14:55:52 +0400

    Implemented EmployeeService

---
+++ src/main/java/com/codeline/library/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.codeline.library.service;
+
+import com.codeline.library.entity.Employee;
+import com.codeline.library.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
