7d2d6f5d18fbfeffbdf49e062ae5e1c111afbcfa
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 16:48:14 +0400

    Created BorrowService skeleton

--- src/main/java/com/codeline/library/service/BorrowService.java
+++ src/main/java/com/codeline/library/service/BorrowService.java
@@ -1,4 +1,32 @@
 package com.codeline.library.service;
 
+import com.codeline.library.repository.BorrowRecordRepository;
+import com.codeline.library.repository.EmployeeRepository;
+import com.codeline.library.repository.ResourceRepository;
+import com.codeline.library.repository.ReservationRepository;
+import com.codeline.library.repository.WaitlistEntryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BorrowService {
+
+    private final EmployeeRepository employeeRepository;
+    private final ResourceRepository resourceRepository;
+    private final BorrowRecordRepository borrowRecordRepository;
+    private final WaitlistEntryRepository waitlistEntryRepository;
+    private final ReservationRepository reservationRepository;
+
+    public BorrowService(
+            EmployeeRepository employeeRepository,
+            ResourceRepository resourceRepository,
+            BorrowRecordRepository borrowRecordRepository,
+            WaitlistEntryRepository waitlistEntryRepository,
+            ReservationRepository reservationRepository) {
+
+        this.employeeRepository = employeeRepository;
+        this.resourceRepository = resourceRepository;
+        this.borrowRecordRepository = borrowRecordRepository;
+        this.waitlistEntryRepository = waitlistEntryRepository;
+        this.reservationRepository = reservationRepository;
+    }
 }
