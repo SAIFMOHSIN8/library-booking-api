0dfa9d3ea6d40a4a85852d93a6bd12e3e2686f78
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 15:17:43 +0400

    Implemented EmployeeController

---
+++ src/main/java/com/codeline/library/service/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.codeline.library.controller;
+
+import com.codeline.library.entity.Employee;
+import com.codeline.library.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
