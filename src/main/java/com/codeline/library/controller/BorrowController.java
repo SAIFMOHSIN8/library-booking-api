76c419b06883bb201f2fd2abd9fdda0c2dd29fdb
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 17:11:43 +0400

    Implemented borrow endpoint

---
+++ src/main/java/com/codeline/library/controller/BorrowController.java
@@ -0,0 +1,24 @@
+package com.codeline.library.controller;
+
+import com.codeline.library.entity.BorrowRecord;
+import com.codeline.library.service.BorrowService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/borrow")
+public class BorrowController {
+
+    private final BorrowService borrowService;
+
+    public BorrowController(BorrowService borrowService) {
+        this.borrowService = borrowService;
+    }
+
+    @PostMapping
+    public BorrowRecord borrowResource(
+            @RequestParam Long employeeId,
+            @RequestParam Long resourceId) {
+
+        return borrowService.borrowResource(employeeId, resourceId);
+    }
+}
