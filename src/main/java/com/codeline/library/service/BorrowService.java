75106c41220fc67417f0093272ffdf71f049f9d1
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-06-22 16:53:56 +0400

    Implemented borrowResource method

--- src/main/java/com/codeline/library/service/BorrowService.java
+++ src/main/java/com/codeline/library/service/BorrowService.java
@@ -1,11 +1,16 @@
 package com.codeline.library.service;
 
+import com.codeline.library.entity.BorrowRecord;
+import com.codeline.library.entity.Employee;
+import com.codeline.library.entity.Resource;
 import com.codeline.library.repository.BorrowRecordRepository;
 import com.codeline.library.repository.EmployeeRepository;
 import com.codeline.library.repository.ResourceRepository;
 import com.codeline.library.repository.ReservationRepository;
 import com.codeline.library.repository.WaitlistEntryRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class BorrowService {
@@ -28,5 +33,23 @@ public class BorrowService {
         this.borrowRecordRepository = borrowRecordRepository;
         this.waitlistEntryRepository = waitlistEntryRepository;
         this.reservationRepository = reservationRepository;
+    }
+
+    public BorrowRecord borrowResource(Long employeeId, Long resourceId) {
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+
+        Resource resource = resourceRepository.findById(resourceId)
+                .orElseThrow(() -> new RuntimeException("Resource not found"));
+
+        BorrowRecord borrowRecord = new BorrowRecord();
+
+        borrowRecord.setEmployee(employee);
+        borrowRecord.setResource(resource);
+        borrowRecord.setBorrowedAt(LocalDateTime.now());
+        borrowRecord.setActive(true);
+
+        return borrowRecordRepository.save(borrowRecord);
     }
 }
